73f64d1c8755cef02c3613d90fe65596168af27f
zincsweet <1099214139@qq.com>
2026-09-08 15:05:54 +0800

    将线程封装到了CP模型中

--- test_thread11/main.cpp
+++ test_thread11/main.cpp
@@ -1,38 +1,35 @@
 #include "BlockQueue.hpp"
 #include <unistd.h>
+#include <memory>
 #include "thread.hpp"
 
 
 
-void* ConsumerRoutine(void* args) {
-    BlockQueue<int>* bq = static_cast<BlockQueue<int>*>(args);
-    while (true) {
-        sleep(5);
-        int data;
-        bq->Pop(&data);
-        std::cout << "消费：" << data << std::endl;
-    }
-}
-
-void* ProductorRoutine(void* args) {
-    BlockQueue<int>* bq = static_cast<BlockQueue<int>*>(args);
-    int data = 10;
-    while (true) {
-        bq->Enqueue(data);
-        std::cout << "生产：" << data++ << std::endl;
-    }
-}
-
-
 int main() {
-    BlockQueue<int>* bq = new BlockQueue<int>();
-    pthread_t c, p;
-    pthread_create(&c, nullptr, ConsumerRoutine, bq);
-    pthread_create(&p, nullptr, ProductorRoutine, bq);
+    std::unique_ptr<BlockQueue<int> > bq = std::make_unique<BlockQueue<int> >();
 
+    my::Thread comsumer([&bq](){
+        while (true) {
+            sleep(1);
+            int data;
+            bq->Pop(&data);
+            std::cout << "消费：" << data << std::endl;
+        }
+    });
 
+    my::Thread productor([&bq](){
+        int data = 10;
+        while (true) {
+            bq->Enqueue(data);
+            std::cout << "生产：" << data++ << std::endl;
+        }
+    });
 
-    pthread_join(c, nullptr);
-    pthread_join(p, nullptr);
+    comsumer.Start();
+    productor.Start();
+
+    comsumer.Join();
+    productor.Join();
+
     return 0;
 }
