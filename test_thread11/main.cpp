784144cae4b13d7ea46ddcd4ec950b7e6b3494bc
zincsweet <1099214139@qq.com>
2026-09-08 19:44:13 +0800

    封装了任务

--- test_thread11/main.cpp
+++ test_thread11/main.cpp
@@ -1,27 +1,40 @@
 #include "BlockQueue.hpp"
 #include <unistd.h>
 #include <memory>
+#include <ctime>
 #include "thread.hpp"
+#include "Task.hpp"
 
 
 
 int main() {
-    std::unique_ptr<BlockQueue<int> > bq = std::make_unique<BlockQueue<int> >();
+    srand(time(nullptr) ^ getpid());
+    std::unique_ptr<BlockQueue<Task> > bq = std::make_unique<BlockQueue<Task> >();
 
     my::Thread comsumer([&bq](){
         while (true) {
             sleep(1);
-            int data;
-            bq->Pop(&data);
-            std::cout << "消费：" << data << std::endl;
+            Task t;
+            // 1. 取数据
+            bq->Pop(&t);
+            // 2. 处理数据
+            // t.Execute();
+            t();
+            std::cout << "消费：" << t.getResult() << std::endl;
         }
     });
 
     my::Thread productor([&bq](){
-        int data = 10;
         while (true) {
-            bq->Enqueue(data);
-            std::cout << "生产：" << data++ << std::endl;
+            // 1. 获取数据
+            int datax = rand() % 10 + 1;
+            usleep(rand() % 1357);
+            int datay = rand() % 10 + 1;
+
+            // 2. 生产过程
+            Task t(datax, datay);
+            bq->Enqueue(t);
+            std::cout << "生产：" << t.Question() << std::endl;
         }
     });
 
