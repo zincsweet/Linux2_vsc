4278abcbca75a42b89946e5bd652d3c8ffeb5eb3
zincsweet <1099214139@qq.com>
2026-09-09 21:24:16 +0800

    引入任务类型之后的环形队列

--- test_sem01/RingQueue.hpp
+++ test_sem01/RingQueue.hpp
@@ -14,7 +14,7 @@ class RingQueue
 public:
     RingQueue(int cap = defalt_cap)
     :_cap(cap),
-     _rp(cap),
+     _rq(cap),
      _consumer_step(0),
      _productor_step(0),
      _blank_sem(cap),
@@ -28,9 +28,9 @@ public:
         _blank_sem.P();
 
         {
-            LockGuard lg(_pmutex)
+            LockGuard lg(_pmutex);
             // 2. 找位置生产
-            _rp[_productor_step++] = in;
+            _rq[_productor_step++] = in;
             _productor_step %= _cap;
         }
         // 3. 给出数据资源
@@ -44,7 +44,7 @@ public:
         {
             LockGuard lg(_cmutex);
             // 2. 把数据弹出
-            *out = _rp[_consumer_step++];
+            *out = _rq[_consumer_step++];
             _consumer_step %= _cap;
         }
         // 3. 给出空白资源
@@ -53,7 +53,7 @@ public:
 
 private:
     int _cap;           // 环形队列的容量
-    std::vector<T> _rp; // 环形队列
+    std::vector<T> _rq; // 环形队列
 
     int _consumer_step; // 消费位置
     int _productor_step;// 生产位置
--- test_sem01/main.cpp
+++ test_sem01/main.cpp
@@ -1,69 +1,88 @@
 #include "RingQueue.hpp"
 #include <unistd.h>
 #include "Task.hpp"
+#include <ctime>
+#include <cstdlib>
 
-Mutex cnt_lock;
-Mutex screen_lock;
+// Mutex cnt_lock;
+// Mutex screen_lock;
 
-int data = 1;
+// int data = 1;
 
-int GetData() {
-    cnt_lock.Lock();
-    int result = data++;
-    cnt_lock.UnLock();
-    return result;
-}
+// int GetData() {
+//     cnt_lock.Lock();
+//     int result = data++;
+//     cnt_lock.UnLock();
+//     return result;
+// }
 
-void Print(const std::string& info) {
-    screen_lock.Lock();
-    std::cout << info << std::endl;
-    screen_lock.UnLock();
-}
+// void Print(const std::string& info) {
+//     screen_lock.Lock();
+//     std::cout << info << std::endl;
+//     screen_lock.UnLock();
+// }
 
 void* ConsumerRoutine(void* args) {
-    RingQueue<int>* rq = static_cast<RingQueue<int>*>(args);
+    // RingQueue<int>* rq = static_cast<RingQueue<int>*>(args);
+    RingQueue<Task>* rq = static_cast<RingQueue<Task>*>(args);
 
     int data = 0;
     while (true) {
+        // sleep(1);
+        // rq->Pop(&data);
+        // Print("消费数据：" + std::to_string(data));
+
         sleep(1);
-        rq->Pop(&data);
-        Print("消费数据：" + std::to_string(data));
+        Task t;
+        rq->Pop(&t);
+        t();
+        std::cout << "消费并完成任务：" << t.getResult() << std::endl;
     }
 }
 
 void* ProductorRoutine(void* args) {
-    RingQueue<int>* rq = static_cast<RingQueue<int>*>(args);
+    // RingQueue<int>* rq = static_cast<RingQueue<int>*>(args);
+    RingQueue<Task>* rq = static_cast<RingQueue<Task>*>(args);
 
     while (true) {
-        int data = GetData();
-        rq->Enqueue(data);
-        Print("生产数据：" + std::to_string(data));
+        // int data = GetData();
+        // rq->Enqueue(data);
+        // Print("生产数据：" + std::to_string(data));
+        
+        int x = rand() % 10 + 1;
+        usleep(135);
+        int y = rand() % 10 + 1;
+        Task t(x, y);
+        rq->Enqueue(t);
+        std::cout << "生产任务：" << t.Question() << std::endl;
     }
 }
 
 int main() {
-    RingQueue<int>* rq = new RingQueue<int>();
+    srand(time(nullptr)^getpid());
+    // RingQueue<int>* rq = new RingQueue<int>();
+    RingQueue<Task>* rq = new RingQueue<Task>();
 
     // 单单
-    // pthread_t c, p;
-    // pthread_create(&c, nullptr, ConsumerRoutine, rq);
-    // pthread_create(&p, nullptr, ProductorRoutine, rq);
+    pthread_t c, p;
+    pthread_create(&c, nullptr, ConsumerRoutine, rq);
+    pthread_create(&p, nullptr, ProductorRoutine, rq);
 
-    // pthread_join(c, nullptr);
-    // pthread_join(p, nullptr);
+    pthread_join(c, nullptr);
+    pthread_join(p, nullptr);
 
     // 多多
-    pthread_t c[2], p[3];
-    pthread_create(c, nullptr, ConsumerRoutine, rq);
-    pthread_create(c+1, nullptr, ConsumerRoutine, rq);
-    pthread_create(p, nullptr, ProductorRoutine, rq);
-    pthread_create(p+1, nullptr, ProductorRoutine, rq);
-    pthread_create(p+2, nullptr, ProductorRoutine, rq);
+    // pthread_t c[2], p[3];
+    // pthread_create(c, nullptr, ConsumerRoutine, rq);
+    // pthread_create(c+1, nullptr, ConsumerRoutine, rq);
+    // pthread_create(p, nullptr, ProductorRoutine, rq);
+    // pthread_create(p+1, nullptr, ProductorRoutine, rq);
+    // pthread_create(p+2, nullptr, ProductorRoutine, rq);
 
-    pthread_join(c[0], nullptr);
-    pthread_join(c[1], nullptr);
-    pthread_join(p[0], nullptr);
-    pthread_join(p[1], nullptr);
-    pthread_join(p[2], nullptr);
+    // pthread_join(c[0], nullptr);
+    // pthread_join(c[1], nullptr);
+    // pthread_join(p[0], nullptr);
+    // pthread_join(p[1], nullptr);
+    // pthread_join(p[2], nullptr);
     return 0;
 }
